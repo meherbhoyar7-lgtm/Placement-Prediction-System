6b47a7dc33267d36b36992a632fff0ed181d06ad
meherbhoyar7-lgtm <meherbhoyar7@gmail.com>
2026-09-18 14:42:23 +0530

    Fix ModuleNotFoundError when running app.py

--- App/app.py
+++ App/app.py
@@ -1,5 +1,9 @@
-import os
 import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 
 import pandas as pd
 import joblib
